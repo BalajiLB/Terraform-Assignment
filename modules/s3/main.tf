42d332b33129e9c205eeb4576fab252ece21edf9
logabalaji.kannaiyan <logabalaji.kannaiyan@example.com>
2025-07-05 09:16:01 +0530

    fix: add

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -34,13 +34,14 @@ resource "aws_kms_key" "s3_kms" {
     {
       "Sid": "Enable IAM User Permissions",
       "Effect": "Allow",
-      "Principal": { "AWS": "*" },
+      "Principal": { { "AWS": "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root" }, },
       "Action": "kms:*",
       "Resource": "*"
     }
   ]
 }
 EOF
+
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
