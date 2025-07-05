4cdc87616ccefbac19f15cb83794b1d6088cb875
logabalaji.kannaiyan <logabalaji.kannaiyan@example.com>
2025-07-05 06:25:13 +0530

    fix(terraform): remove duplicate variable definitions and clean up variables.tf

--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -1,6 +1,6 @@
 # Environment and Region
 variable "env" {
-  description = "Environment name"
+  description = "Environment name (e.g., dev, prod)"
   type        = string
 }
 
@@ -46,86 +46,71 @@ variable "instance_type" {
   type        = string
 }
 
-# S3 Variables
-variable "bucket_name" {
-  description = "Name of the S3 bucket"
-  type        = string
-}
-
-# Security Group - Split Ingress Variables
-variable "ingress_descriptions" {
-  type        = list(string)
-  description = "Descriptions for each ingress rule"
-}
-
-variable "ingress_from_ports" {
-  type        = list(number)
-  description = "From ports for each ingress rule"
-}
-
-variable "ingress_to_ports" {
-  type        = list(number)
-  description = "To ports for each ingress rule"
-}
-
-variable "ingress_cidr_blocks" {
-  type        = list(list(string))
-  description = "CIDR blocks for each ingress rule"
-}
-
-# Security Group - Egress Variables
-variable "egress_from_port" {
-  type        = number
-  description = "Egress from port"
-}
-
-variable "egress_to_port" {
-  type        = number
-  description = "Egress to port"
-}
-
-variable "egress_protocol" {
-  type        = string
-  description = "Egress protocol"
-}
-
-variable "egress_cidr_blocks" {
-  type        = list(string)
-  description = "Egress CIDR blocks"
-}
-
-# Common Tags
-variable "tags" {
-  type        = map(string)
-  description = "Common tags applied to all resources"
-}
-
 variable "ec2_role_name" {
   description = "IAM Role name to attach to EC2 instance profile"
   type        = string
 }
 
-variable "env" {
-  type        = string
-  description = "Environment name (e.g., dev, prod)"
-}
-
+# S3 Variables
 variable "bucket_name" {
-  type        = string
   description = "Name of the main S3 bucket (without environment prefix)"
+  type        = string
 }
 
 variable "replication_target_bucket" {
-  type        = string
   description = "The name of the destination bucket for cross-region replication"
+  type        = string
 }
 
 variable "logging_target_bucket" {
-  type        = string
   description = "The S3 bucket where access logs will be delivered"
+  type        = string
 }
 
+# Security Group - Split Ingress Variables
+variable "ingress_descriptions" {
+  description = "Descriptions for each ingress rule"
+  type        = list(string)
+}
+
+variable "ingress_from_ports" {
+  description = "From ports for each ingress rule"
+  type        = list(number)
+}
+
+variable "ingress_to_ports" {
+  description = "To ports for each ingress rule"
+  type        = list(number)
+}
+
+variable "ingress_cidr_blocks" {
+  description = "CIDR blocks for each ingress rule"
+  type        = list(list(string))
+}
+
+# Security Group - Egress Variables
+variable "egress_from_port" {
+  description = "Egress from port"
+  type        = number
+}
+
+variable "egress_to_port" {
+  description = "Egress to port"
+  type        = number
+}
+
+variable "egress_protocol" {
+  description = "Egress protocol"
+  type        = string
+}
+
+variable "egress_cidr_blocks" {
+  description = "Egress CIDR blocks"
+  type        = list(string)
+}
+
+# Common Tags
 variable "tags" {
+  description = "Common tags applied to all resources"
   type        = map(string)
-  description = "A map of tags to assign to the resources"
 }
